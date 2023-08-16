6b6cd83b40b938ae3e63fac2c3bf5456ecf9ab40
Alex Zhuravlev <leftemail17@mail.ru>
2023-08-16 20:14:13 +0300

    adding retention for mapper annotation

--- src/main/java/com/greenblat/socialmedia/util/Mapper.java
+++ src/main/java/com/greenblat/socialmedia/util/Mapper.java
@@ -1,11 +1,19 @@
 package com.greenblat.socialmedia.util;
 
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Component
 @Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
 public @interface Mapper {
+    @AliasFor(
+            annotation = Component.class
+    )
+    String value() default "";
 }
