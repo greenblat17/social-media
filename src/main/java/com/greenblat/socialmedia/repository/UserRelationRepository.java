6e7739f84695070ff7f2ab1a14f366f3686c956e
Alex Zhuravlev <leftemail17@mail.ru>
2023-08-18 10:15:30 +0300

    adding repo for user relation (#4)

---
+++ src/main/java/com/greenblat/socialmedia/repository/UserRelationRepository.java
@@ -0,0 +1,17 @@
+package com.greenblat.socialmedia.repository;
+
+import com.greenblat.socialmedia.model.User;
+import com.greenblat.socialmedia.model.UserRelation;
+import com.greenblat.socialmedia.model.UserRelationId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRelationRepository extends JpaRepository<UserRelation, UserRelationId> {
+
+    void deleteByRelatingUserAndFollowingUser(User relatingUser, User followingUser);
+
+    Optional<UserRelation> findByRelatingUserAndFollowingUser(User relatingUser, User followingUser);
+}
