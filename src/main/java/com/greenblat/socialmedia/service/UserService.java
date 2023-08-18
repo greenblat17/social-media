54399d037525c86b88693236c5290da40f5c0bcf
Alex Zhuravlev <leftemail17@mail.ru>
2023-08-18 13:53:14 +0300

    implementing unfollow and accept friend request functionality (#3)

--- src/main/java/com/greenblat/socialmedia/service/UserService.java
+++ src/main/java/com/greenblat/socialmedia/service/UserService.java
@@ -1,34 +1,102 @@
 package com.greenblat.socialmedia.service;
 
 import com.greenblat.socialmedia.model.RelationType;
+import com.greenblat.socialmedia.model.User;
 import com.greenblat.socialmedia.model.UserRelation;
 import com.greenblat.socialmedia.repository.UserRelationRepository;
 import com.greenblat.socialmedia.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final UserRelationRepository userRelationRepository;
 
+    @Transactional
     public void followUser(Long followingUserId, UserDetails userDetails) {
-        var user = userRepository.findByUsername(userDetails.getUsername())
-                .orElseThrow();
-        var followingUser = userRepository.findById(followingUserId)
-                .orElseThrow();
+        var user = findUser(userDetails.getUsername());
+        var followingUser = findUser(followingUserId);
 
-        var userRelation = UserRelation.builder()
-                .relatingUser(user)
-                .followingUser(followingUser)
-                .relationType(RelationType.FOLLOWER)
-                .build();
+        var followingUserToCurrentUserRelation = userRelationRepository
+                .findByRelatingUserAndFollowingUser(followingUser, user);
 
+        if (followingUserToCurrentUserRelation.isPresent()) {
+            acceptFriendRequest(user, followingUser, followingUserToCurrentUserRelation.get());
+        } else {
+            var userRelation = buildUserRelation(user, followingUser, RelationType.FOLLOWER);
+            userRelationRepository.save(userRelation);
+        }
+    }
+
+    @Transactional
+    public void requestToFriend(Long friendRequestId,
+                                UserDetails userDetails,
+                                boolean isFriend) {
+        if (isFriend) {
+            var user = findUser(userDetails.getUsername());
+            var followingUser = findUser(friendRequestId);
+
+            var userRelation = userRelationRepository
+                    .findByRelatingUserAndFollowingUser(followingUser, user)
+                    .orElseThrow();
+
+            if (userRelation.getRelationType().equals(RelationType.FRIEND)) {
+                throw new RuntimeException();
+            }
+
+            acceptFriendRequest(user, followingUser, userRelation);
+        }
+    }
+
+    @Transactional
+    public void unfollowUser(Long followingUserId, UserDetails userDetails) {
+        var user = findUser(userDetails.getUsername());
+        var followingUser = findUser(followingUserId);
+        userRelationRepository.deleteByRelatingUserAndFollowingUser(user, followingUser);
+
+        userRelationRepository
+                .findByRelatingUserAndFollowingUser(followingUser, user)
+                .ifPresent(userRelation -> updateUserRelation(userRelation, RelationType.FOLLOWER));
+    }
+
+    private void acceptFriendRequest(User currentUser,
+                                     User userToFriend,
+                                     UserRelation followingUserToCurrentUserRelation) {
+        updateUserRelation(followingUserToCurrentUserRelation, RelationType.FRIEND);
+
+        var userRelation = buildUserRelation(currentUser, userToFriend, RelationType.FRIEND);
         userRelationRepository.save(userRelation);
     }
 
+    private void updateUserRelation(UserRelation userRelation, RelationType relationType) {
+        userRelation.setRelationType(relationType);
+        userRelationRepository.save(userRelation);
+    }
+
+    private User findUser(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow();
+    }
+
+    private User findUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow();
+    }
+
+    private static UserRelation buildUserRelation(User currentUser,
+                                                  User followingUser,
+                                                  RelationType relationType) {
+        return UserRelation.builder()
+                .relatingUser(currentUser)
+                .followingUser(followingUser)
+                .relationType(relationType)
+                .build();
+    }
 }
