9471fe518650c686b43e6676bc58a8b2270f4469
Alex Zhuravlev <leftemail17@mail.ru>
2023-08-18 10:14:14 +0300

    adding constructors to entity (#4)

--- src/main/java/com/greenblat/socialmedia/model/UserRelation.java
+++ src/main/java/com/greenblat/socialmedia/model/UserRelation.java
@@ -1,9 +1,17 @@
 package com.greenblat.socialmedia.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "user_relation")
 @IdClass(UserRelationId.class)
