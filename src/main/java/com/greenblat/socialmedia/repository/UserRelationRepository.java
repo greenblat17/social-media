41f541ad6595ea4acc5e3b5ece12fcb2ded97d9c
Alex Zhuravlev <leftemail17@mail.ru>
2023-08-19 11:53:13 +0300

    adding method for finding relation by user (#5)

--- src/main/java/com/greenblat/socialmedia/repository/UserRelationRepository.java
+++ src/main/java/com/greenblat/socialmedia/repository/UserRelationRepository.java
@@ -6,6 +6,7 @@ import com.greenblat.socialmedia.model.UserRelationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface UserRelationRepository extends JpaRepository<UserRelation, User
     void deleteByRelatingUserAndFollowingUser(User relatingUser, User followingUser);
 
     Optional<UserRelation> findByRelatingUserAndFollowingUser(User relatingUser, User followingUser);
+
+    List<UserRelation> findByRelatingUserId(Long relatingUserId);
 }
